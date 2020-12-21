6cbaf50b3fb5e0bd666f572f01f8adc3a2f0a490
pastelmind <keepyourhonor@gmail.com>
2020-12-21 23:29:04 +0900

    feat: Add sinceKolmafiaRevision()

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,40 @@
-console.log('Try npm run lint/fix!');
+/**
+ * @file Utilities for writing JavaScript code that runs in KoLmafia.
+ */
 
-const longString =
-  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ut aliquet diam.';
+import {getRevision} from 'kolmafia';
 
-const trailing = 'Semicolon';
-
-const why = 'am I tabbed?';
-
-export function doSomeStuff(
-  withThis: string,
-  andThat: string,
-  andThose: string[]
-) {
-  //function on one line
-  if (!andThose.length) {
-    return false;
+/**
+ * Represents an exception thrown when the current KoLmafia version does not
+ * match an expected condition.
+ */
+export class KolmafiaVersionError extends Error {
+  constructor(message?: string) {
+    super(message);
   }
-  console.log(withThis);
-  console.log(andThat);
-  console.dir(andThose);
-  return;
 }
-// TODO: more examples
+
+/**
+ * If KoLmafia's revision number is less than `revision`, throws an exception.
+ * Otherwise, does nothing.
+ *
+ * This behaves like the `since rXXX;` statement in ASH.
+ * @param revision Revision number
+ * @throws {KolmafiaVersionError}
+ *    If KoLmafia's revision number is less than `revision`.
+ * @throws {TypeError} If `revision` is not an integer
+ */
+export function sinceKolmafiaRevision(revision: number): void {
+  if (!Number.isInteger(revision)) {
+    throw new TypeError(
+      `Invalid revision number ${revision} (must be an integer)`
+    );
+  }
+
+  // Based on net.sourceforge.kolmafia.textui.Parser.sinceException()
+  if (getRevision() < revision) {
+    throw new KolmafiaVersionError(
+      `This script requires revision r${revision} of kolmafia or higher (current: ${getRevision()}). Up-to-date builds can be found at https://ci.kolmafia.us/.`
+    );
+  }
+}
