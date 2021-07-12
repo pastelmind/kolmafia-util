7c5354d842da11ef42e979660c8cec58fb721eb5
pastelmind <keepyourhonor@gmail.com>
2021-07-12 16:51:01 +0900

    feat!: add assert submodule
    Add a primitive assertion module, loosely inspired by Node.js' built-in
    `assert` module, as well as Chai. This _must_ be imported using
    `kolmafia-util/assert`.
    
    BREAKING CHANGE:
    
    To export two entrypoints, use the `exports` and `typesVersions` fields
    in `package.json`. This requires Node.js >= 12.16.0 || >= 13.7.0.

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,14 +1,24 @@
 import buble from '@rollup/plugin-buble';
 import typescript from '@rollup/plugin-typescript';
 import createBubleConfig from 'buble-config-rhino';
+import * as path from 'path';
 import type {RollupOptions} from 'rollup';
 
 function createConfig(format: 'cjs' | 'esm'): RollupOptions {
   return {
     external: ['kolmafia'],
-    input: 'src/index.ts',
+    input: ['src/index.ts', 'src/assert/index.ts'],
     output: {
       dir: `build/${format}`,
+      entryFileNames: chunkInfo => {
+        // Emit output chunks according to their relative paths of the entry
+        // points in the source dir
+        if (!chunkInfo.facadeModuleId) throw new Error('No facadeModuleId');
+        return path.join(
+          path.relative('src', path.dirname(chunkInfo.facadeModuleId)),
+          '[name].js'
+        );
+      },
       format,
       sourcemap: true,
     },
